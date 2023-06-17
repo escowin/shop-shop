1970ed5dd658fe8a51ca0929ba063a9272ac3e91
escowin <edwin@escowinart.com>
2023-06-16 20:57:18 -0500

    updated global state with products

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,21 +1,36 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
+import React, { useEffect } from "react";
+import { useQuery } from "@apollo/client";
 
-import ProductItem from '../ProductItem';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import spinner from '../../assets/spinner.gif';
+import ProductItem from "../ProductItem";
+import { QUERY_PRODUCTS } from "../../utils/queries";
+import spinner from "../../assets/spinner.gif";
 
-function ProductList({ currentCategory }) {
+// global state & actions
+import { useStoreContext } from "../../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../../utils/actions";
+
+function ProductList() {
+  // retrieves current global state object & dispatch() method to update state. currentCategory is destructured out of state object so it can be used in filterProducts()
+  const [state, dispatch] = useStoreContext();
+  const { currentCategory } = state;
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const products = data?.products || [];
+  // waits for useQuery() response. dispatch() executes when data object goes from an undefined to a defined value. instructs reducer to use UPDATE_PRODUCTS action to save the product data array to gloabl store. useStoreContext() executres again which gives the product data needed to display products to the page.
+  useEffect(() => {
+    if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products,
+      });
+    }
+  }, [data, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
-      return products;
+      return state.products;
     }
 
-    return products.filter(
+    return state.products.filter(
       (product) => product.category._id === currentCategory
     );
   }
@@ -23,7 +38,7 @@ function ProductList({ currentCategory }) {
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {products.length ? (
+      {state.products.length ? (
         <div className="flex-row">
           {filterProducts().map((product) => (
             <ProductItem
