e88b1f297594979257b3012be8ed0b659703108f
escowin <edwin@escowinart.com>
2023-06-18 21:15:40 -0500

    tdd shopping cart reducers

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -4,5 +4,12 @@ export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
 // - takes categories list retrieved from the server by Apollo, stores in global state
 export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
 // - connects piece of data for above actions. ie users select a category from the state created by the `UPDATE_CATEGORIES` action, displays products for that category from the `UPDATE_PRODUCTS` action-created list
-export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY"; 
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
 
+//  shopping cart actions
+export const ADD_TO_CART = "ADD_TO_CART";
+export const ADD_MULTIPLE_TO_CART = "ADD_MULTIPLE_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const UPDATE_CART_QUANTITY = "UPDATE_CART_QUANTITY";
+export const CLEAR_CART = "CLEAR_CART";
+export const TOGGLE_CART = "TOGGLE_CART";
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -5,6 +5,12 @@ import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART,
 } from "./actions";
 
 export const reducer = (state, action) => {
@@ -26,6 +32,52 @@ export const reducer = (state, action) => {
         ...state,
         currentCategory: action.currentCategory,
       };
+    // shopping cart cases
+    case ADD_TO_CART:
+      return {
+        // preserves everything else on `state`
+        ...state,
+        // users can immediately view cart with newly added item if it's not already open
+        cartOpen: true,
+        // updates `cart` property to add `action.product` to the end of array
+        cart: [...state.cart, action.product],
+      };
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter((product) => product._id !== action._id);
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        // uses map to create a new array (state.cart is treated as immutable)
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        }),
+      };
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: [],
+      };
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen,
+      };
     default:
       return state;
   }
@@ -34,5 +86,5 @@ export const reducer = (state, action) => {
 // useReducer | similar to useState(), but used to manage greater levels of state
 // initializes gloabel state object. provides functionality for updating state by automattically running it trough custom reducer().
 export function useProductReducer(initialState) {
-    return useReducer(reducer, initialState);
+  return useReducer(reducer, initialState);
 }
