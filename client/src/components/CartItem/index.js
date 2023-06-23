53c4a77c2400ab98053759593acdc36a4c9ce389
escowin <edwin@escowinart.com>
2023-06-22 22:43:12 -0500

    data persistence is updated in global state & object store

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { useStoreContext } from "../../utils/GlobalState";
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
   // this component does not need to read from state, hence only dispatch() is destructured
   const [, dispatch] = useStoreContext();
+
+  // updates the global state & `cart` object store when an item is removed from the cart
   const removeFromCart = (item) => {
     dispatch({
       type: REMOVE_FROM_CART,
       _id: item._id,
     });
+    // removes item from indexedDb `cart` object store.
+    idbPromise("cart", "delete", { ...item });
   };
 
   const onChange = (e) => {
@@ -19,12 +24,16 @@ const CartItem = ({ item }) => {
         type: REMOVE_FROM_CART,
         _id: item._id,
       });
+
+      idbPromise("cart", "delete", { ...item });
     } else {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: item._id,
         purchaseQuantity: parseInt(value),
       });
+
+      idbPromise("cart", "put", { ...item, purchaseQuantity: parseInt(value) });
     }
   };
 
@@ -39,7 +48,12 @@ const CartItem = ({ item }) => {
         </div>
         <div>
           <span>Qty:</span>
-          <input type="number" placeholder="1" value={item.purchaseQuantity} onChange={onChange} />
+          <input
+            type="number"
+            placeholder="1"
+            value={item.purchaseQuantity}
+            onChange={onChange}
+          />
           <span
             role="img"
             aria-label="trash"
